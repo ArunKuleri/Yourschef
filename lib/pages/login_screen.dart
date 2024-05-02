ce1dad18109ed7aeb82d2799f4c412e9efb075a2
ArunKuleri <arunk.aru1@gmail.com>
2024-05-02 14:13:50 +0530

    added the firebase

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:yourschef/pages/sign_up.dart';
@@ -11,6 +12,31 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String email = "", password = "";
+  TextEditingController useremailController = TextEditingController();
+  TextEditingController userpasswordController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+  userLogin() async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user not found") {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text(
+          "No User Found for that Email",
+          style: TextStyle(fontSize: 18.0, color: Colors.black),
+        )));
+      } else if (e.code == "wrong password") {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text(
+          "Wrong Password Provided by user",
+          style: TextStyle(fontSize: 18.0, color: Colors.black),
+        )));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,103 +68,137 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           Container(
             margin: const EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
-            child: Column(
-              children: [
-                const Padding(padding: EdgeInsets.all(8)),
-                const Center(
-                  child: Text(
-                    "YoursChef",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Poppins"),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50.0,
-                ),
-                Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 2,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-                        Text(
-                          "Login ",
-                          style: AppWidget.HeadLinetextFieldStyle(),
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                              hintText: "Email", prefixIcon: Icon(Icons.mail)),
-                        ),
-                        const SizedBox(
-                          height: 12.0,
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                              hintText: "Password",
-                              prefixIcon: Icon(Icons.lock)),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          alignment: Alignment.topRight,
-                          child: Text(
-                            "Forgot Password?",
-                            style: AppWidget.semiboldTextFeildStyle(),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 80.0,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          width: 200,
-                          decoration: BoxDecoration(
-                              color: const Color(0xfffff5722),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: const Center(
-                            child: Text(
-                              "Login",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18.0,
-                                  fontFamily: "Poppins",
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        )
-                      ],
+            child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  const Padding(padding: EdgeInsets.all(8)),
+                  const Center(
+                    child: Text(
+                      "YoursChef",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Poppins"),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 70.0,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUp()));
-                  },
-                  child: Text(
-                    "Don't have an account? Sign up",
-                    style: AppWidget.semiboldTextFeildStyle(),
+                  const SizedBox(
+                    height: 50.0,
                   ),
-                )
-              ],
+                  Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            "Login ",
+                            style: AppWidget.HeadLinetextFieldStyle(),
+                          ),
+                          TextFormField(
+                            controller: useremailController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please Enter  Password';
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                                hintText: "Email",
+                                prefixIcon: Icon(Icons.mail)),
+                          ),
+                          const SizedBox(
+                            height: 12.0,
+                          ),
+                          TextFormField(
+                            controller: userpasswordController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please Enter Password";
+                              }
+                              return null;
+                            },
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                hintText: "Password",
+                                prefixIcon: Icon(Icons.lock)),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              "Forgot Password?",
+                              style: AppWidget.semiboldTextFeildStyle(),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 80.0,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              if (_formkey.currentState!.validate()) {
+                                setState(() {
+                                  email = useremailController.text;
+                                  password = userpasswordController.text;
+                                });
+                              }
+                              userLogin();
+                            },
+                            child: Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(20),
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                width: 200,
+                                decoration: BoxDecoration(
+                                    color: const Color(0xfffff5722),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                  child: Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.0,
+                                        fontFamily: "Poppins",
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 70.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUp()));
+                    },
+                    child: Text(
+                      "Don't have an account? Sign up",
+                      style: AppWidget.semiboldTextFeildStyle(),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:yourschef/pages/bottomnav.dart';
 import 'package:yourschef/pages/login_screen.dart';
 import 'package:yourschef/widgets/Widgets_support.dart';
 
@@ -10,6 +12,42 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  String email = "", password = "", name = "";
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+  registrtion() async {
+    if (password != null) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+        ScaffoldMessenger.of(context).showSnackBar((const SnackBar(
+            content: Text(
+          "Registered SucessFully",
+          style: TextStyle(fontSize: 20),
+        ))));
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => BottomNav()));
+      } on FirebaseException catch (e) {
+        if (e.code == "weak password") {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text(
+            "Password too weaak ",
+            style: TextStyle(fontSize: 20),
+          )));
+        } else if (e.code == "email already in use") {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              backgroundColor: Colors.orange,
+              content: Text(
+                "Account already exists",
+                style: TextStyle(fontSize: 18),
+              )));
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,108 +79,153 @@ class _SignUpState extends State<SignUp> {
           ),
           Container(
             margin: const EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
-            child: Column(
-              children: [
-                const Padding(padding: EdgeInsets.all(8)),
-                const Center(
-                  child: Text(
-                    "YoursChef",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Poppins"),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50.0,
-                ),
-                Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 1.5,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-                        Text(
-                          "Login ",
-                          style: AppWidget.HeadLinetextFieldStyle(),
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                              hintText: "Name", prefixIcon: Icon(Icons.person)),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                              hintText: "Email", prefixIcon: Icon(Icons.mail)),
-                        ),
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                              hintText: "Password",
-                              prefixIcon: Icon(Icons.lock)),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          alignment: Alignment.topRight,
-                          child: Text(
-                            "Forgot Password?",
-                            style: AppWidget.semiboldTextFeildStyle(),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 80.0,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          width: 200,
-                          decoration: BoxDecoration(
-                              color: const Color(0xfffff5722),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: const Center(
-                            child: Text(
-                              "Sign Up",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18.0,
-                                  fontFamily: "Poppins",
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        )
-                      ],
+            child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  const Padding(padding: EdgeInsets.all(8)),
+                  const Center(
+                    child: Text(
+                      "YoursChef",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Poppins"),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 70.0,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  },
-                  child: Text(
-                    "Already have an account? Login up",
-                    style: AppWidget.semiboldTextFeildStyle(),
+                  const SizedBox(
+                    height: 50.0,
                   ),
-                )
-              ],
+                  Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 1.5,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            "Login ",
+                            style: AppWidget.HeadLinetextFieldStyle(),
+                          ),
+                          TextFormField(
+                            controller: namecontroller,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "please Enter E-mail";
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                                hintText: "Name",
+                                prefixIcon: Icon(Icons.person)),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: emailcontroller,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please Enter Password";
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                                hintText: "Email",
+                                prefixIcon: Icon(Icons.mail)),
+                          ),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          TextFormField(
+                            controller: passwordcontroller,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "please enter the password";
+                              }
+                              return null;
+                            },
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                hintText: "Password",
+                                prefixIcon: Icon(Icons.lock)),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              "Forgot Password?",
+                              style: AppWidget.semiboldTextFeildStyle(),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 80.0,
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              if (_formkey.currentState!.validate()) {
+                                setState(() {
+                                  email = emailcontroller.text;
+                                  name = namecontroller.text;
+                                  password = passwordcontroller.text;
+                                });
+                              }
+                              registrtion();
+                            },
+                            child: Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(20),
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                width: 200,
+                                decoration: BoxDecoration(
+                                    color: const Color(0xfffff5722),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                  child: Text(
+                                    "Sign Up",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.0,
+                                        fontFamily: "Poppins",
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 70.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    },
+                    child: Text(
+                      "Already have an account? Login up",
+                      style: AppWidget.semiboldTextFeildStyle(),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
